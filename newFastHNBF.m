862e4bfd1e99665fd61ae0af97a6f23c0260d359
anonyxxx <42889729+anonyxxx@users.noreply.github.com>
2019-01-19 19:55:10 +0800

    Initialization

--- newFastHNBF.m
+++ newFastHNBF.m
@@ -8,42 +8,42 @@ function newFastHNBF(init_delta, usr_zeros, itm_zeros)
     
     global matX_train
     
-    global G_matTheta        % dim(M, K): latent document-topic intensities
+    global G_matTheta        % dim(M, K): latent user preference
     global G_matTheta_Shp    % dim(M, K): varational param of matTheta (shape)
     global G_matTheta_Rte    % dim(M, K): varational param of matTheta (rate)
 
-    global G_matBeta         % dim(N, K): latent word-topic intensities
+    global G_matBeta         % dim(N, K): latent item attribute
     global G_matBeta_Shp     % dim(N, K): varational param of matBeta (shape)
     global G_matBeta_Rte     % dim(N, K): varational param of matBeta (rate)
     
-    global G_matEpsilon      % dim(M, 1): latent word-topic intensities
+    global G_matEpsilon      % dim(M, 1): latent user activity
     global G_matEpsilon_Shp  % dim(M, 1): varational param of matEpsilon (shape)
     global G_matEpsilon_Rte  % dim(M, 1): varational param of matEpsilon (rate)
 
-    global G_matEta          % dim(N, 1): latent word-topic intensities
+    global G_matEta          % dim(N, 1): latent item popularity
     global G_matEta_Shp      % dim(N, 1): varational param of matEta (shape)
     global G_matEta_Rte      % dim(N, 1): varational param of matEta (rate)
     
-    global G_vecMu           % dim(M, 1): approximate matD
-    global G_vecMu_Shp       % dim(M, 1): approximate matD
-    global G_vecMu_Rte       % dim(M, 1): approximate matD
-    global G_matGamma        % dim(M, 1): latent word-topic intensities
-    global G_matGamma_Shp    % dim(M, 1): varational param of matEpsilonX (shape)
-    global G_matGamma_Rte    % dim(M, 1): varational param of matEpsilonX (rate)
+    global G_vecMu           % dim(M, 1): latent exposure factor
+    global G_vecMu_Shp       % dim(M, 1): varational param of G_vecMu (shape)
+    global G_vecMu_Rte       % dim(M, 1): varational param of G_vecMu (shape)
+    global G_matGamma        % dim(M, K): latent user dispersion factor
+    global G_matGamma_Shp    % dim(M, K): varational param of G_matGamma (shape)
+    global G_matGamma_Rte    % dim(M, K): varational param of G_matGamma (rate)
 
-    global G_vecPi           % dim(N, 1): approximate matD
-    global G_vecPi_Shp       % dim(N, 1): approximate matD
-    global G_vecPi_Rte       % dim(N, 1): approximate matD
-    global G_matDelta        % dim(N, 1): latent word-topic intensities
-    global G_matDelta_Shp    % dim(N, 1): varational param of matEtaX (shape)
-    global G_matDelta_Rte    % dim(N, 1): varational param of matEtaX (rate)
+    global G_vecPi           % dim(N, 1): latent exposure factor
+    global G_vecPi_Shp       % dim(N, 1): varational param of G_vecPi (shape)
+    global G_vecPi_Rte       % dim(N, 1): varational param of G_vecPi (rate)
+    global G_matDelta        % dim(N, K): latent item dispersion factor
+    global G_matDelta_Shp    % dim(N, K): varational param of G_matDelta (shape)
+    global G_matDelta_Rte    % dim(N, K): varational param of G_matDelta (rate)
     
+    global vec_matR_ui
     global vec_matR_ui_shp
     global vec_matR_ui_rte
-    global vec_matR_ui
+    global vec_matD_ui
     global vec_matD_ui_shp
     global vec_matD_ui_rte
-    global vec_matD_ui
     
     global G_prior
     
--- test_FastHNBF.m
+++ test_FastHNBF.m
@@ -9,35 +9,35 @@ global Best_matTheta
 global Best_matBeta
  
 % ----- HNBF -----
-global G_matTheta        % dim(M, K): latent document-topic intensities
+global G_matTheta        % dim(M, K): latent user preference
 global G_matTheta_Shp    % dim(M, K): varational param of matTheta (shape)
 global G_matTheta_Rte    % dim(M, K): varational param of matTheta (rate)
 
-global G_matBeta         % dim(N, K): latent word-topic intensities
+global G_matBeta         % dim(N, K): latent item attribute
 global G_matBeta_Shp     % dim(N, K): varational param of matBeta (shape)
 global G_matBeta_Rte     % dim(N, K): varational param of matBeta (rate)
 
-global G_matEpsilon      % dim(M, 1): latent word-topic intensities
+global G_matEpsilon      % dim(M, 1): latent user activity
 global G_matEpsilon_Shp  % dim(M, 1): varational param of matEpsilon (shape)
 global G_matEpsilon_Rte  % dim(M, 1): varational param of matEpsilon (rate)
 
-global G_matEta          % dim(N, 1): latent word-topic intensities
+global G_matEta          % dim(N, 1): latent item popularity
 global G_matEta_Shp      % dim(N, 1): varational param of matEta (shape)
 global G_matEta_Rte      % dim(N, 1): varational param of matEta (rate)
 
-global G_vecMu           % dim(M, 1): approximate matD
-global G_vecMu_Shp       % dim(M, 1): approximate matD
-global G_vecMu_Rte       % dim(M, 1): approximate matD
-global G_matGamma        % dim(M, K): approximate matD
-global G_matGamma_Shp    % dim(M, K): approximate matD
-global G_matGamma_Rte    % dim(M, K): approximate matD
+global G_vecMu           % dim(M, 1): latent exposure factor
+global G_vecMu_Shp       % dim(M, 1): varational param of G_vecMu (shape)
+global G_vecMu_Rte       % dim(M, 1): varational param of G_vecMu (shape)
+global G_matGamma        % dim(M, K): latent user dispersion factor
+global G_matGamma_Shp    % dim(M, K): varational param of G_matGamma (shape)
+global G_matGamma_Rte    % dim(M, K): varational param of G_matGamma (rate)
 
-global G_vecPi           % dim(N, 1): approximate matD
-global G_vecPi_Shp       % dim(N, 1): approximate matD
-global G_vecPi_Rte       % dim(N, 1): approximate matD
-global G_matDelta        % dim(N, K): approximate matD
-global G_matDelta_Shp    % dim(N, K): approximate matD
-global G_matDelta_Rte    % dim(N, K): approximate matD
+global G_vecPi           % dim(N, 1): latent exposure factor
+global G_vecPi_Shp       % dim(N, 1): varational param of G_vecPi (shape)
+global G_vecPi_Rte       % dim(N, 1): varational param of G_vecPi (rate)
+global G_matDelta        % dim(N, K): latent item dispersion factor
+global G_matDelta_Shp    % dim(N, K): varational param of G_matDelta (shape)
+global G_matDelta_Rte    % dim(N, K): varational param of G_matDelta (rate)
 
 global vec_matR_ui_shp
 global vec_matR_ui_rte
@@ -64,7 +64,7 @@ global matPrecNRecall
 
 NUM_RUNS = 10;
 likelihood_step = 10;
-check_step = 50;
+check_step = 10;
 
 ini_scale = 0.001;
 Ks = [20];
@@ -72,8 +72,8 @@ topK = [5, 10, 15, 20];
 MaxItr = 400;
 G_prior = [3, 1, 0.1, ...
            3, 1, 0.1, ...
-           1e2, 1e7, 1e2, 1e7, ...
-           200, 1e5];
+           1e2, 1e8, 1e2, 1e8, ...
+           200, 1e4];
     
 matPrecNRecall = zeros(NUM_RUNS*length(Ks), length(topK)*6);
 
@@ -114,7 +114,7 @@ for kk = 1:length(Ks)
     %% Model initialization
     %
     G_prior(1:6) = [30, 1*K, 0.1*sqrt(K), ...
-                        30, 1*K, 0.1*sqrt(K)];
+                    30, 1*K, 0.1*sqrt(K)];
     newFastHNBF(ini_scale, usr_zeros, itm_zeros);
     
     [is_X_train, js_X_train, vs_X_train] = find(matX_train);
@@ -150,16 +150,10 @@ for kk = 1:length(Ks)
             else
                 user_probe = usr_idx;
             end
-            if strcmp(DATA, 'MovieLens100K') || strcmp(DATA, 'MovieLens1M') || strcmp(DATA, 'ML100KPos')
-                [valid_precision(indx,:), valid_recall(indx,:), valid_nDCG(indx,:), valid_MRR(indx,:)] = Evaluate_ALL(matX_valid(user_probe,:), matX_train(user_probe,:), ...
-                                                                                                                      G_matTheta(user_probe,:), G_matBeta, topK);
-                fprintf('validation nDCG: %f\n', valid_nDCG(indx,1));
-            else
-                [valid_precision(indx,:), valid_recall(indx,:), valid_MRR(indx,:)] = Evaluate_PrecNRec(matX_valid(user_probe,:), matX_train(user_probe,:), ...
-                                                                                                       G_matTheta(user_probe,:), G_matBeta, topK);
-            end
-            fprintf('validation precision: %f\n', valid_precision(indx,1));
-            fprintf('validation recall: %f\n', valid_recall(indx,1));
+            [valid_precision(indx,:), valid_recall(indx,:), valid_MRR(indx,:)] = Evaluate_PrecNRec(matX_valid(user_probe,:), matX_train(user_probe,:), ...
+                                                                                                   G_matTheta(user_probe,:), G_matBeta, topK);
+            fprintf('validation precision@5: %f\n', valid_precision(indx,1));
+            fprintf('validation recall@5   : %f\n', valid_recall(indx,1));
             
             % Calculate the metrics on testing set
             fprintf('Testing ... \n');
@@ -169,28 +163,12 @@ for kk = 1:length(Ks)
             else
                 user_probe = 1:length(usr_idx);
             end
-            if strcmp(DATA, 'MovieLens100K') || strcmp(DATA, 'MovieLens1M') || strcmp(DATA, 'ML100KPos')
-                [test_precision(indx,:), test_recall(indx,:), test_nDCG(indx,:), test_MRR(indx,:)] = Evaluate_ALL(matX_test(user_probe,:)+matX_valid(user_probe,:), matX_train(user_probe,:), ...
-                                                                                                                  G_matTheta(user_probe,:), G_matBeta, topK);
-                fprintf('testing nDCG: %f\n', test_nDCG(indx,1));
-            else
-                [test_precision(indx,:), test_recall(indx,:), test_MRR(indx,:)] = Evaluate_PrecNRec(matX_test(user_probe,:)+matX_valid(user_probe,:), matX_train(user_probe,:), ...
-                                                                                                    G_matTheta(user_probe,:), G_matBeta, topK);
-            end
-            fprintf('testing precision: %f\n', test_precision(indx,1));
-            fprintf('testing recall: %f\n', test_recall(indx,1));
-            
-            % Draw a consumption sample 
-            range = min(N, 100);
-            index = 30;
-            tmp1 = G_matBeta(1:range,:) * G_matTheta(index,:)';
-            tmp2 = G_matDelta(1:range,:) * G_matGamma(index,:)' / K;
-            js_sparse = js_X_train(is_X_train == index);
-            D_sparse = vec_matD_ui(is_X_train == index);
-            js_sparse_range = js_sparse(js_sparse<=range);
-            D_sparse_range = D_sparse(js_sparse<=range);
-            tmp2(js_sparse_range) = D_sparse_range;
-            plot(full([tmp1 tmp1.*tmp2 matX_train(index,1:range)' matX_test(index,1:range)']));
+            [test_precision(indx,:), test_recall(indx,:), test_MRR(indx,:)] = Evaluate_PrecNRec(matX_test(user_probe,:)+matX_valid(user_probe,:), matX_train(user_probe,:), ...
+                                                                                                G_matTheta(user_probe,:), G_matBeta, topK);
+            fprintf('testing precision@5 : %f\n', test_precision(indx,1));
+            fprintf('testing precision@10: %f\n', test_precision(indx,2));
+            fprintf('testing recall@5    : %f\n', test_recall(indx,1));
+            fprintf('testing recall@10   : %f\n', test_recall(indx,2));
         end
     
         if itr >= MaxItr
@@ -198,21 +176,17 @@ for kk = 1:length(Ks)
         end               
     end
     
-    if valid_poisson(end,1) > valid_poisson(end-1,1)
-        Best_matTheta = G_matTheta;
-        Best_matBeta = G_matBeta;
-    end
     Best_matTheta = G_matTheta;
     Best_matBeta = G_matBeta;
     
-    if strcmp(DATA, 'MovieLens100K') || strcmp(DATA, 'MovieLens1M') || strcmp(DATA, 'ML100KPos')
-        [total_test_precision, total_test_recall, total_test_nDCG, total_test_MRR] = Evaluate_ALL(matX_test(usr_idx,:)+matX_valid(usr_idx,:), matX_train(usr_idx,:), ...
-                                                                                                  Best_matTheta(usr_idx,:), Best_matBeta, topK);
-        fprintf('testing nDCG: %f\n', total_test_nDCG);
-    else
-        [total_test_precision, total_test_recall, total_test_MRR] = Evaluate_PrecNRec(matX_test(usr_idx,:)+matX_valid(usr_idx,:), matX_train(usr_idx,:), ...
-                                                                                      Best_matTheta(usr_idx,:), Best_matBeta, topK);
-    end
+    [total_test_precision, total_test_recall, total_test_MRR] = Evaluate_PrecNRec(matX_test(usr_idx,:)+matX_valid(usr_idx,:), matX_train(usr_idx,:), ...
+                                                                                  Best_matTheta(usr_idx,:), Best_matBeta, topK);
+    fprintf('\n -------------------- final test --------------------\n');
+    fprintf('testing precision@5 : %f\n', total_test_precision(1,1));
+    fprintf('testing precision@10: %f\n', total_test_precision(1,2));
+    fprintf('testing recall@5    : %f\n', total_test_recall(1,1));
+    fprintf('testing recall@10   : %f\n', total_test_recall(1,2));
+    fprintf('testing MRR         : %f\n', total_test_MRR(1,1));
 end
 
 
